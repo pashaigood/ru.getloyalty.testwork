f828771c27960434a726a35954d7e82d3e72b26d
Pavel Belugin <pashaigood@gmail.com>
2015-10-08 14:40:05 +0300

    Рефакторинг
    * Убрана бизнес логика из контроллера.

---
+++ src/app/components/data/data.factory.js
@@ -0,0 +1,212 @@
+(function () {
+    angular
+        .module('testWork')
+        .factory('dataFactory', data);
+
+    /** @ngInject */
+    function data($filter) {
+
+        var orderBy = $filter('orderBy');
+
+        return {
+            getData: function () {
+                return [
+                    // First data set
+                    [
+                        {
+                            Name: 'Item 1',
+                            Percent: '20,4'
+                        }
+                    ],
+                    // Second data set
+                    [
+                        {
+                            Name: 'Item 1',
+                            Percent: 20
+                        },
+                        {
+                            Name: 'Item 2',
+                            Percent: 40
+                        }
+                    ],
+                    // Third data set
+                    [
+                        {
+                            Name: 'Item 1',
+                            Percent: 20
+                        },
+                        {
+                            Name: 'Item 2',
+                            Percent: 30
+                        },
+                        {
+                            Name: 'Item 3',
+                            Percent: 50
+                        }
+                    ],
+                    // Fourth data set
+                    [
+                        {
+                            Name: 'Item 1',
+                            Percent: '20,67'
+                        },
+                        {
+                            Name: 'Item 2',
+                            Percent: '40.45'
+                        },
+                        {
+                            Name: 'Item 3',
+                            Percent: 60
+                        },
+                        {
+                            Name: 'Item 4',
+                            Percent: 0
+                        }
+                    ]
+                ];
+            },
+
+            /**
+             * Метод нормализует набор данных.
+             * При передачи второго параметра, нормализация
+             * происходит относительно переданного элемента.
+             *
+             * @example
+             * // В Наборе есть данные проценты: [20, 30, 50, 0]
+             * // Пользователь изменил 4 параметр на 90
+             * normalize(vm.dataSet, vm.dataSet[2]);
+             * // В этом случаи значение 4 параметра останется 90,
+             * // а в общем значения будут выглядеть как [10, 0, 0, 90].
+             *
+             * @param {Array<Object>} dataSet
+             * @param {Object} element
+             */
+            normalize: function (dataSet, element) {
+                var self = this,
+                    sum = 0,
+                    coeff,
+                    normalizedPart = element ? 100 - element.Percent : 100;
+
+                // Найдём всё количество процентов.
+                angular.forEach(dataSet, function (elem) {
+                    if (element !== elem) {
+                        sum += elem.Percent;
+                    }
+                });
+
+
+                if (normalizedPart === sum) {
+                    return;
+                }
+                // Найдём коэффицент.
+                coeff = sum > 0 ? normalizedPart / sum : 0;
+
+                // Приведём общее значение к 100.
+                angular.forEach(dataSet, function (elem) {
+                    if (element !== elem) {
+                        elem.Percent *= coeff;
+                    }
+                    self.finalizeElement(elem);
+                });
+            },
+
+            /**
+             * Метод финализирует элемент после обработки.
+             * @param {Object} elem
+             */
+            finalizeElement: function (elem) {
+                elem.Percent = Math.round(elem.Percent * 100) / 100;
+                elem._Percent = elem.Percent;
+            },
+
+            /**
+             * Метод формтаирует проценты элемента, преобразует для работы.
+             * @param {Object} elem
+             */
+            format: function (elem) {
+                if (angular.isString(elem.Percent)) {
+                    elem.Percent = elem.Percent.replace(',', '.');
+                    elem.Percent = parseFloat(elem.Percent);
+                }
+            },
+
+            /**
+             * Метод перерасчитывает текущий набор данных,
+             * так чтобы их значение не привышало 100.
+             * @param {Array<Object>} dataSet
+             * @param {Object} elem
+             */
+            recalculate: function (dataSet, elem) {
+                elem.Percent = elem.Percent > 0 ? elem.Percent : 0;
+                elem.Percent = elem.Percent < 100 ? elem.Percent : 100;
+
+                var value = elem.Percent,
+                    oldValue = elem._Percent,
+                    changedElement,
+                    order = [];
+
+                // Если значение уменьшилось,
+                if (value < oldValue) {
+
+                    // то это количество передается первому элементу,
+                    // с наименьшим количеством.
+                    order = orderBy(dataSet, ['Percent', 'Name']);
+
+                }
+                else
+                // Если значение увеличилось,
+                if (value > oldValue) {
+                    // то это количество компенсируется первым
+                    // элементом с наибольшим количеством.
+                    order = orderBy(dataSet, ['-Percent', 'Name']);
+                }
+                else {
+                    return;
+                }
+
+
+                var cnt = order.length;
+                while ((cnt -= 1) > -1) {
+
+                    while ((changedElement = order.shift()) === elem) {
+                        // Убедимся, что элемент не являеться сам сабой.
+                    }
+
+                    if (changedElement) {
+                        var newValue = changedElement.Percent + (oldValue - value);
+
+                        // Если значение получиться меньше нуля,
+                        // То отнимаем только то, что до нуля.
+                        // Перём следующий элемент из сортировки и проделываем тоже самое с ним.
+
+                        if (newValue < 0) {
+                            // Найдём количество процентов,
+                            // которые ещё не компенсировали.
+                            // И компенсируем его у следующего элемента.
+                            oldValue = value + newValue;
+                            changedElement.Percent = 0;
+                            this.finalizeElement(changedElement);
+                            continue;
+
+                        }
+                        else if (newValue > 100) {
+                            changedElement.Percent = 100;
+                        }
+                        else {
+                            changedElement.Percent = newValue;
+                        }
+
+                        this.finalizeElement(changedElement);
+                        this.finalizeElement(elem);
+                        break;
+
+                    }
+
+                }
+
+            }
+
+
+        };
+    }
+})();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,76 +6,16 @@
         .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController($filter, data) {
-        var vm = this,
-            orderBy = $filter('orderBy');
-
+    function MainController(dataFactory) {
+        var vm = this;
 
         function init() {
-            vm.data = data;
-            if (data.length) {
+            var data = vm.data = dataFactory.getData();
+            if (vm.data.length) {
                 vm.selectSet(data[data.length - 1]);
             }
         }
 
-        /**
-         * Метод формтаирует проценты элемента, преобразует для работы.
-         * @param {Object} elem
-         */
-        function format(elem) {
-            if (angular.isString(elem.Percent)) {
-                elem.Percent = elem.Percent.replace(',', '.');
-                elem.Percent = parseFloat(elem.Percent);
-            }
-        }
-
-        function finalizeElement(elem) {
-            elem.Percent = Math.round(elem.Percent * 100) / 100;
-            elem._Percent = elem.Percent;
-        }
-
-        /**
-         * Метод нормализует набор данных.
-         * При передачи второго параметра, нормализация
-         * происходит относительно переданного элемента.
-         *
-         * @example
-         * // В Наборе есть данные процентов: [20, 30, 50, 0]
-         * // Пользователь изменил 4 параметр на 90
-         * normalize(vm.dataSet, vm.dataSet[2]);
-         * // В этом случаи значение 4 параметра останется 90,
-         * // а в общем значения будут выглядеть как [4, 6, 0, 90].
-         *
-         * @param {Array<Object>} dataSet
-         * @param {Object} element
-         */
-        function normalize(dataSet, element) {
-            var sum = 0,
-                coeff,
-                normalizedPart = element ? 100 - element.Percent : 100;
-
-            // Найдём всё количество процентов.
-            angular.forEach(dataSet, function (elem) {
-                if (element !== elem) {
-                    sum += elem.Percent;
-                }
-            });
-
-
-            if (normalizedPart === sum) {
-                return;
-            }
-            // Найдём коэффицент.
-            coeff = sum > 0 ? normalizedPart / sum : 0;
-
-            // Приведём общее значение к 100.
-            angular.forEach(dataSet, function (elem) {
-                if (element !== elem) {
-                    elem.Percent *= coeff;
-                }
-                finalizeElement(elem);
-            });
-        }
 
         /**
          * Метод выбирает набор данных.
@@ -84,9 +24,9 @@
         vm.selectSet = function (dataSet) {
             vm.dataSet = dataSet;
             angular.forEach(dataSet, function (elem) {
-                format(elem);
+                dataFactory.format(elem);
             });
-            normalize(vm.dataSet);
+            dataFactory.normalize(vm.dataSet);
         };
 
         /**
@@ -94,83 +34,11 @@
          * @param {Object} elem
          */
         vm.change = function (elem) {
-            format(elem);
-            recalculate(elem);
-            normalize(vm.dataSet, elem);
+            dataFactory.format(elem);
+            dataFactory.recalculate(vm.dataSet, elem);
+            dataFactory.normalize(vm.dataSet, elem);
         };
 
-        /**
-         * Метод перерасчитывает текущий набор данных, так чтобы их значение не привышало 100
-         */
-        function recalculate(elem) {
-            elem.Percent = elem.Percent > 0 ? elem.Percent : 0;
-            elem.Percent = elem.Percent < 100 ? elem.Percent : 100;
-
-            var value = elem.Percent,
-                oldValue = elem._Percent,
-                changedElement,
-                order = [];
-
-            // Если значение уменьшилось,
-            if (value < oldValue) {
-
-                // то это количество передается первому элементу,
-                // с наименьшим количеством.
-                order = orderBy(vm.dataSet, ['Percent', 'Name']);
-
-            }
-            else
-            // Если значение увеличилось,
-            if (value > oldValue) {
-                // то это количество компенсируется первым
-                // элементом с наибольшим количеством.
-                order = orderBy(vm.dataSet, ['-Percent', 'Name']);
-            }
-            else {
-                return;
-            }
-
-
-            var cnt = order.length;
-            while ((cnt -= 1) > -1) {
-
-                while ((changedElement = order.shift()) === elem) {
-                    // Убедимся, что элемент не являеться сам сабой.
-                }
-
-                if (changedElement) {
-                    var newValue = changedElement.Percent + (oldValue - value);
-
-                    // Если значение получиться меньше нуля,
-                    // То отнимаем только то, что до нуля.
-                    // Перём следующий элемент из сортировки и проделываем тоже самое с ним.
-
-                    if (newValue < 0) {
-                        // Найдём количество процентов,
-                        // которые ещё не компенсировали.
-                        // И компенсируем его у следующего элемента.
-                        oldValue = value + newValue;
-                        changedElement.Percent = 0;
-                        finalizeElement(changedElement);
-                        continue;
-
-                    }
-                    else if (newValue > 100) {
-                        changedElement.Percent = 100;
-                    }
-                    else {
-                        changedElement.Percent = newValue;
-                    }
-
-                    finalizeElement(changedElement);
-                    finalizeElement(elem);
-                    break;
-
-                }
-
-            }
-
-        }
 
 
         init();
